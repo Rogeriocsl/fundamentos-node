a7ea92d4954b9e7d910d965663f58548ebaa604d
Rogério carlos <carlosrog1414@hotmail.com>
2021-02-22 19:11:44 -0300

    finally

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,13 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionsDTO {
+  title: string,
+  value: number,
+  type: 'income' | 'outcome'
+}
+
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +21,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const {income, outcome} = this.transactions.reduce((acumulator: Balance, transaction: Transaction) => {
+      switch ( transaction.type){
+        case "income":
+          acumulator.income += transaction.value;
+          break;
+        case "outcome":
+          acumulator.outcome += transaction.value;
+          break;
+        default:
+          break;
+      }
+      return acumulator;
+    },
+    {
+      income: 0,
+      outcome:0,
+      total: 0,
+    },
+  );
+
+  const total = income - outcome;
+
+  return {income, outcome, total};
+
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: CreateTransactionsDTO): Transaction {
+    const transaction = new Transaction({title, value, type});
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
